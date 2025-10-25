5cc0b01b0c822943eda9bf78f88f591014e0ee1a
ugrek <romanvetrin@gmail.com>
2025-10-25 19:38:54 +0200

    [+] added yandex cli

--- rig-core/src/client/builder.rs
+++ rig-core/src/client/builder.rs
@@ -171,8 +171,8 @@ impl<'a> DynClientBuilder {
             ),
             ClientFactory::new(
                 DefaultProviders::YANDEX,
-                yandex::Client::<reqwest::Client>::from_env_boxed,
-                yandex::Client::<reqwest::Client>::from_val_boxed,
+                yandex::client::Client::<reqwest::Client>::from_env_boxed,
+                yandex::client::Client::<reqwest::Client>::from_val_boxed,
             ),
         ])
     }
